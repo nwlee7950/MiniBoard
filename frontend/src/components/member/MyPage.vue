cf5eb2d7b135499934988837c49c095dc6502338
choijisu <cjscoding1997@gmail.com>
2021-12-10 15:50:35 +0900

    Add: MyPage publishing

--- frontend/src/components/member/MyPage.vue
+++ frontend/src/components/member/MyPage.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="my_page">mypage</div>
+<div class="mypage">
+    <div class="title">My Page</div>
+    <div class="container">
+        <span>이름</span>
+        <span></span>
+        <span>등급</span>
+        <span></span>
+        <span>아이디</span>
+        <span></span>
+        <span>이메일</span>
+        <span></span>
+    </div>
+</div>
 </template>
 
 <script>
@@ -8,6 +20,25 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.mypage {
+    padding-top: 100px;
+    display: flex;
+    flex-direction: column;
+}
 
+.title {
+    font-size: 70px;
+    width: 100%;
+    margin: 60px 0;
+    text-decoration: underline;
+    text-align: center;
+}
+.container {
+    background-color: var(--white-color);
+    padding: 50px;
+    border-radius: 5px;
+    box-shadow: 2px 2px 10px var(--shadow-color);
+    height: 60vh;
+}
 </style>
