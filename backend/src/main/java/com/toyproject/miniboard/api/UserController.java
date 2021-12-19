2dc8518a4c8dffa85c0af2099bdefa8154dcb32b
이승현 <nwlee7950@naver.com>
2021-12-19 22:40:00 +0900

    add Login

--- backend/src/main/java/com/toyproject/miniboard/api/UserController.java
+++ backend/src/main/java/com/toyproject/miniboard/api/UserController.java
@@ -1,18 +1,25 @@
 package com.toyproject.miniboard.api;
 
-import com.toyproject.miniboard.model.dto.BoardParameterDto;
+import com.toyproject.miniboard.jwt.JwtFilter;
+import com.toyproject.miniboard.jwt.TokenProvider;
 import com.toyproject.miniboard.model.dto.UserDto;
+import com.toyproject.miniboard.model.dto.UserLoginDto;
 import com.toyproject.miniboard.model.dto.UserRegisterDto;
 import com.toyproject.miniboard.model.service.UserService;
-import com.toyproject.miniboard.model.vo.Board;
+import com.toyproject.miniboard.model.vo.Token;
 import com.toyproject.miniboard.model.vo.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -26,6 +33,8 @@ import java.util.List;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     @ApiOperation(value = "회원 가입을 한다.", response = String.class)
     @PostMapping()
@@ -33,6 +42,27 @@ public class UserController {
         log.info("register user : {}", userRegisterDto);
         userService.register(userRegisterDto);
         return new ResponseEntity<String>(HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "로그인", response = Token.class)
+    @PostMapping("/login")
+    public ResponseEntity<Token> login(@Valid @RequestBody UserLoginDto userLoginDto) throws Exception{
+        log.info("call login : {}", userLoginDto);
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(userLoginDto.getId(), userLoginDto.getPassword());
+
+        //유저 정보를 조회하여 인증 정보를 생성
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        //해당 인증 정보를 현재 실행중인 스레드(Security Context)에 저장
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        //해당 인증 정보를 기반으로 jwt 토큰을 생성
+        String jwt = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        //생성된 Token을 Response Header에 넣고, Token vo 객체를 이용해 Response Body에도 넣어서 리턴
+        return new ResponseEntity<Token>(new Token(jwt), httpHeaders, HttpStatus.OK);
     }
 
     @ApiOperation(value = "회원 정보를 수정한다.", response = String.class)
--- backend/src/main/java/com/toyproject/miniboard/jwt/JwtFilter.java
+++ backend/src/main/java/com/toyproject/miniboard/jwt/JwtFilter.java
@@ -14,6 +14,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
+//JwtFilter : API에 요청이 오게되면 헤더에 토큰이 있나없나 확인
 @Slf4j
 @RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
