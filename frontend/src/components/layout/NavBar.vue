ac469ae9c842bc66da4b309948446bdc151b4a26
choijisu <cjscoding1997@gmail.com>
2021-12-30 22:41:04 +0900

    Add: user logout

--- frontend/src/components/layout/NavBar.vue
+++ frontend/src/components/layout/NavBar.vue
@@ -1,22 +1,43 @@
 <template>
-  <div class="navbar">
-      <router-link :to="{name:'Home'}" class="logo link"><i class="fas fa-stream"></i> Mini Board</router-link>
-      <div class="menus">
-          <router-link :to="{name:'Board'}" class="board link"><i class="far fa-keyboard"></i> Board</router-link>
-          <router-link :to="{name:'MyPage'}" class="mypage link"><i class="fas fa-user-cog"></i> My Page</router-link>
-          <router-link :to="{name:'MemberForm'}" class="dropdown link"><i class="far fa-user-circle"></i></router-link>
-      </div>
-  </div>
+<div class="navbar">
+    <router-link :to="{name:'Home'}" class="logo link"><i class="fas fa-stream"></i> Mini Board</router-link>
+    <div class="menus">
+        <router-link :to="{name:'Board'}" class="board link"><i class="far fa-keyboard"></i> Board</router-link>
+        <div class="in" v-if="isLogin">
+            <router-link :to="{name:'MyPage'}" class="mypage link"><i class="far fa-user-circle"></i> My Page</router-link>
+            <button @click.prevent="logoutUser"><i class="far fa-user-circle"></i>로그아웃</button>
+        </div>
+        <div class="out" v-else>
+            <router-link :to="{name:'MemberForm'}" class="dropdown link"><i class="far fa-user-circle"></i>Login / Join</router-link>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
+import {
+    mapState,
+    mapMutations,
+} from 'vuex';
 export default {
-    name:'NavBar',
+    name: 'NavBar',
+    computed: {
+        ...mapState("memberStore", ["isLogin"]),
+    },
+    methods: {
+        ...mapMutations("memberStore", ["SET_IS_LOGIN", "SET_USER_INFO"]),
+        logoutUser() {
+            this.SET_IS_LOGIN(false);
+            this.SET_USER_INFO(null);
+            sessionStorage.removeItem("access-token");
+            
+        },
+    }
 }
 </script>
 
 <style>
-.navbar{
+.navbar {
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -25,21 +46,26 @@ export default {
     background-color: var(--white-color);
     height: 90px;
 }
-.logo{
+
+.logo {
     font-size: 30px;
 }
-.menus{
+
+.menus {
     display: flex;
 }
-.link{
+
+.link {
     margin-right: 50px;
     text-decoration: none;
     color: var(--font-color);
 }
-.link:hover{
+
+.link:hover {
     color: var(--main-color);
 }
-.dropdown{
+
+.dropdown {
     position: relative;
 }
 </style>
