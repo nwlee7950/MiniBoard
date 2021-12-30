b856bc0323038370bf3fa2d8fcbafd8a9d08bbc6
choijisu <cjscoding1997@gmail.com>
2021-12-31 04:58:03 +0900

    Update: join page publishing
    join api 추후 수정

--- frontend/src/components/member/child/Join.vue
+++ frontend/src/components/member/child/Join.vue
@@ -7,7 +7,8 @@
         <input type="text" name="pw" placeholder="비밀번호 입력..." v-model="user.password">
         <label for="name">Name</label>
         <input type="text" name="name" placeholder="이름 입력..." v-model="user.name">
-        <button @click="registUser">회원가입</button>
+        <input type="file" @change="previewFiles">
+        <button @click="registUser" class="joinBtn">회원가입</button>
     </form>
 </div>
 </template>
@@ -26,8 +27,8 @@ export default {
                 id: "",
                 name: "",
                 password: "",
-                profileImage: "",
-            }
+            },
+            file: null,
         }
     },
     computed: {
@@ -36,10 +37,14 @@ export default {
     methods: {
         ...mapActions("memberStore", ["userRegister"]),
         async registUser() {
-            await this.userRegister(this.user);
+            await this.userRegister(this.user, this.file);
             this.$router.push({
                 name: "Login"
             })
+        },
+        previewFiles(event) {
+            let file = event.target.files[0];
+            this.file = file;
         }
     }
 
@@ -47,5 +52,39 @@ export default {
 </script>
 
 <style scoped>
+.join {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 60vh;
+}
 
+form {
+    display: flex;
+    flex-direction: column;
+    background-color: rgb(226, 224, 226);
+    border-radius: 10px;
+    padding: 20px 50px;
+    font-size: 20px;
+    justify-content: center;
+    align-items: center;
+}
+
+input {
+    outline: none;
+    width: 300px;
+    border: none;
+    margin: 15px 0;
+    padding: 5px;
+    border-radius: 3px;
+}
+.joinBtn{
+    width: 100px;
+    border: none;
+    border-radius: 3px;
+    background-color: var(--main-color);
+    color: white;
+    margin-top: 15px;
+    padding: 3px 0;
+}
 </style>
